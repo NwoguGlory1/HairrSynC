e04d7ba326c32ac19ebe19bce355fb80b6d8935c
NwoguGlory1 <glorynwogu18@gmail.com>
2024-08-08 00:47:21 +0100

    Working on Product model

--- hairsync/store/models.py
+++ hairsync/store/models.py
@@ -7,7 +7,6 @@ from django.db import models
 #     # All fields below are already taken care of and the commented fields are taken care of too 
 #     pass  # Use pass to indicate that this is a placeholder for future code
 
-# Product model
 # Category model
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
@@ -18,6 +17,20 @@ class Category(models.Model):
     image = models.ImageField(upload_to='category_images/', blank=True, null=True)
 
     def __str__(self):
+        return self.name
+
+# Product model
+class Product(models.Model):
+   id = models.AutoField(primary_key=True)
+   name = models.CharField(max_length=255)
+   description = models.TextField(blank=True, null=True)
+   slug = models.SlugField(max_length=255, unique=True) 
+   image = models.ImageField(upload_to='product_images/', blank=True, null=True)
+   price = models.DecimalField(max_digits=10, decimal_places=2)
+   categories = models.ManyToManyField('Category')
+#    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, related_name='products')
+
+   def __str__(self):
         return self.name
 # Order model
 # Cart model
