886e9381de712142c0dfafcea98fb2dd4c3ba1e4
NwoguGlory1 <glorynwogu18@gmail.com>
2024-09-19 23:34:36 +0100

    Working on sign up

--- hairsync/store/forms.py
+++ hairsync/store/forms.py
@@ -19,4 +19,5 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField(max_length=200)
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2',)
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
