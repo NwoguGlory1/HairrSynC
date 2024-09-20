dcbceafa094af69e04fad40386f5b6db89ad866c
NwoguGlory1 <glorynwogu18@gmail.com>
2024-09-20 18:45:54 +0100

    All placeholders in signup.html is showing now

--- hairsync/store/forms.py
+++ hairsync/store/forms.py
@@ -13,11 +13,33 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms.widgets import EmailInput, PasswordInput
 
 class SignUpForm(UserCreationForm):
-    # pass
-    email = forms.EmailField(max_length=200)
+    password1 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2 = forms.CharField(max_length=16, widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password confirmation'}))
+    
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-        
+        widgets = {
+            'username': forms.TextInput(attrs={
+                        'class':'form-control',
+                        'placeholder':'Username',
+                }), 
+            'first_name': forms.TextInput(attrs={
+                        'class':'form-control',
+                        'placeholder':'First name',
+                }), 
+            'last_name': forms.TextInput(attrs={
+                        'class':'form-control',
+                        'placeholder':'Last name',
+                }),
+            'email': EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email adress'}),
+        }
+        # password1 and password2 are declare on the UserCreationForm (they aren't model fields), therefore you can't use them in widgets
+        def __init__(self, *args, **kwargs):
+            super(SignUpForm, self).__init__(*args, **kwargs)
+            self.fields['email'].widget = forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email address'})
+            self.fields['password1'].widget = PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+            self.fields['password2'].widget = PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password confirmation'})
