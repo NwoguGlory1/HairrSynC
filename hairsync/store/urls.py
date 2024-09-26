60735495ccea12d19bceb3925c5778a82c0606df
NwoguGlory1 <glorynwogu18@gmail.com>
2024-09-26 01:33:53 +0100

    Working on Forgot Password feature

--- hairsync/store/urls.py
+++ hairsync/store/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-#from django.contrib.auth import views as auth_views
+from django.contrib.auth import views as auth_views
 
 # app_name = "store"
 urlpatterns = [
@@ -17,7 +17,18 @@ urlpatterns = [
     path('category/<slug:category_slug>/', views.category_products, name='category_products'),
 
     path('signup/', views.signup, name='signup'), 
-    path('login/', views.login_view, name='login'), 
+    path('login/', views.login_view, name='login'),
+
+    # Forgot password URL
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    # Page shown after submitting email for password reset
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    # URL from the password reset email for the user to enter a new password
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # Page shown after the user successfully resets their password
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+
     path('logout/', views.logout_view, name='logout'), 
 
     path('cart/', views.cart, name='cart'),
--- hairsync/store/views.py
+++ hairsync/store/views.py
@@ -16,6 +16,11 @@ def index(request):
 def home(request):
     return render(request, 'store/home.html')
 
+def password_reset_form(request, )
+
+
+
+
 def category_products(request, category_slug):
     category = get_object_or_404(Category, slug=category_slug) #Gets all info about category
     # products = Product.objects.filter(category=category) used if you have category field in product model
