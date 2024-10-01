c24eaa98fdee8687a57d340a57c9a1b07f4431a2
NwoguGlory1 <glorynwogu18@gmail.com>
2024-10-01 23:49:31 +0100

    Edited apps.py

--- hairsync/store/apps.py
+++ hairsync/store/apps.py
@@ -4,3 +4,8 @@ from django.apps import AppConfig
 class StoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "store"
+
+    # add this
+    # What we did is override the ready() method of the users app config to perform initialization task which is registering signals
+    def ready(self):
+        import store.signals  # nameofapp.signals
